3de7cebb6e1235d8e84157a68ac3f8ad7311e2e7
Your Name <you@example.com>
2022-08-30 21:17:58 +0300

    1

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿//   Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+//  Console.WriteLine("Введите A: ");
+//    int A = int.Parse(Console.ReadLine());
+//    Console.WriteLine("Введите B: ");
+//    int B = int.Parse(Console.ReadLine());
+
+//    void FindDegree(int A, int B)
+//    {
+//     int result = 1;
+//     for ( int i =1; i<= B; i++)
+//     {
+//         result = result * A;
+//     }
+//       Console.WriteLine(result);
+//    }
+//     FindDegree( A,B );
